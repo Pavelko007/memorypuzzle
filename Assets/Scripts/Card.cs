3de9ad88604dac43080c373560286d08d6ca2ae1
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-19 10:34:28 +0300

    make a flippable card prefab

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public GameObject FrontFace;
+    public GameObject BackFace;
+
+    private Canvas frontCanvas;
+    private Canvas backCanvas;
+
+    private CardOrientation curOrientation = CardOrientation.Front;
+
+    enum CardOrientation
+    {
+        Front,
+        Back
+    }
+
+    void Awake()
+    {
+        frontCanvas = FrontFace.GetComponent<Canvas>();
+        backCanvas = BackFace.GetComponent<Canvas>();
+    }
+
+    [ContextMenu("flip card")]
+    public void FlipCard()
+    {
+        switch (curOrientation)
+        {
+            case CardOrientation.Front:
+                SetOrientation(CardOrientation.Back);
+                break;
+            case CardOrientation.Back:
+                SetOrientation(CardOrientation.Front);
+                break;
+        }
+    }
+
+    void SetOrientation(CardOrientation newOrientation)
+    {
+        switch (newOrientation)
+        {
+            case CardOrientation.Back:
+                transform.rotation = Quaternion.Euler(new Vector3(180, 0, 0));
+                backCanvas.sortingOrder = 1;
+                frontCanvas.sortingOrder = -1;
+                break;
+            case CardOrientation.Front:
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+                frontCanvas.sortingOrder = 1;
+                backCanvas.sortingOrder = -1;
+                break;
+        }
+        curOrientation = newOrientation;
+    }
+}
